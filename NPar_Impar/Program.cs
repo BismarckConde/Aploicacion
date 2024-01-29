9156660d3ac57a51bb9d152efdb37fd438bc1b9d
Sistemas2-1 <Sistemas2-1@DESKTOP-F6DE7AT>
2024-01-29 16:28:12 -0600

    Agregar archivos de proyecto.

---
+++ NPar_Impar/Program.cs
@@ -0,0 +1,31 @@
+﻿int Numero;
+Console.Title = "VERIFICANDO NUMEROS PARES";
+Console.Write("\nIngrese un numero:");
+Numero = int.Parse(Console.ReadLine());
+if (Numero < 0)
+{
+    Console.Write("\n\tNumero Negativo...Ingrese un numero positivo");
+
+
+}
+if (Numero == 0)
+{
+    Console.WriteLine("\n La division entre cero no existe ");
+}
+else if (Numero % 2 == 0)
+{
+    Console.Write("\n\tEl numero (" + Numero + ") es par.");
+    if (Numero >= 10 && Numero <= 100)
+    {
+        Console.Write("\n\tEl numero (" + Numero + ") se encuentra en el rango [10-100]");
+    }
+    else
+    {
+        Console.Write("\n\tEl numero (" + Numero + ") NO esta en el rango [10-100]");
+    }
+}
+else
+{
+    Console.Write("\n\tEl numero ingresado es impar...");
+}
+Console.ReadKey();
